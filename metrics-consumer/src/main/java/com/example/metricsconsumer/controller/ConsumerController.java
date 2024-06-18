d4068b4d01391c86eee857b7aa4b277c3cacd2a0
Evgenii.Pichugov <eugepic@yandex.ru>
2024-06-18 15:43:49 +0300

    api docs

--- metrics-consumer/src/main/java/com/example/metricsconsumer/controller/ConsumerController.java
+++ metrics-consumer/src/main/java/com/example/metricsconsumer/controller/ConsumerController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "API for getting metrics")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/metrics")
--- metrics-producer/src/main/java/com/example/metricsproducer/controller/MetricsProducerController.java
+++ metrics-producer/src/main/java/com/example/metricsproducer/controller/MetricsProducerController.java
@@ -1,13 +1,19 @@
 package com.example.metricsproducer.controller;
 
 import com.example.metricsproducer.service.ProducerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "API for sending metrics")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
@@ -15,6 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class MetricsProducerController {
     private final ProducerService producerService;
 
+    @Operation(summary = "Send random metrics")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Metrics been sent")
+    })
+    @GetMapping
     @PostMapping
     public ResponseEntity<String> sendMetrics() {
         producerService.sendRandom();
