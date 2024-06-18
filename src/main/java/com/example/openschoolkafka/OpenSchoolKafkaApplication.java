77b5d54a7a39239ec218d661e23f969ac6313262
Evgenii.Pichugov <eugepic@yandex.ru>
2024-06-18 11:19:13 +0300

    initial commit

---
+++ src/main/java/com/example/openschoolkafka/OpenSchoolKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.openschoolkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSchoolKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenSchoolKafkaApplication.class, args);
+    }
+
+}
